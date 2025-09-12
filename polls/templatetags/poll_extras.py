e009953a7ea40e58630813df2841756537720dc6
nave64 <36815738+nave64@users.noreply.github.com>
2025-09-12 14:06:25 +0330

    font changes and more

--- polls/templatetags/poll_extras.py
+++ polls/templatetags/poll_extras.py
@@ -4,6 +4,18 @@ from jalali_date import date2jalali
 register = template.Library()
 
 
+def to_persian_numbers(text):
+    """Convert English numbers to Persian numbers"""
+    persian_digits = '۰۱۲۳۴۵۶۷۸۹'
+    english_digits = '0123456789'
+    
+    # Create translation table
+    translation_table = str.maketrans(english_digits, persian_digits)
+    
+    # Convert numbers in the text
+    return str(text).translate(translation_table)
+
+
 @register.filter(name='cut')
 def cut(value, arg):
     """Removes all values of arg from the given string"""
@@ -17,15 +29,22 @@ def show_jalali_date(value):
 
 @register.filter(name='three_digits_currency')
 def three_digits_currency(value: int):
-    return '{:,}'.format(value) + ' تومان'
+    formatted_value = '{:,}'.format(value) + ' تومان'
+    return to_persian_numbers(formatted_value)
 
 
 @register.filter(name='discount_price')
 def discount_polls(value: int):
-    formatted_value = '{:,.0f}'.format(float(value))  # Format the value with commas
-    return f'{formatted_value} تومان'
+    formatted_value = '{:,.0f}'.format(float(value)) + ' تومان'  # Format the value with commas
+    return to_persian_numbers(formatted_value)
 
 
+
+
+@register.filter(name='persian_numbers')
+def persian_numbers(value):
+    """Convert any text with English numbers to Persian numbers"""
+    return to_persian_numbers(value)
 
 
 @register.simple_tag
